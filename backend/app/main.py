9f47bf756f2b271d9fd696abf20502855ef3bebd
BkDALI <dalibk005@gmail.com>
2026-07-28 19:18:44 +0100

    Test CORS configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,14 +17,10 @@ app = FastAPI(
 # CORS configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://bkdali.github.io",
-        "http://localhost:5500",
-        "http://127.0.0.1:5500"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
+    allow_credentials=False,
 )
 
 
