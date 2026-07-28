3840e7f6763c10c4c7a6d8d4f40100e4e82798a1
BkDALI <dalibk005@gmail.com>
2026-07-28 18:52:08 +0100

    Use Render backend URL

--- frontend/js/calculator.js
+++ frontend/js/calculator.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://carbon-footprint-api.onrender.com";
 
 const session = typeof getSession === "function" ? getSession() : null;
 if (!session) window.location.href = "connexion.html";
